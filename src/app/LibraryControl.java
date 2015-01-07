724ea2208d44f8b2657ca3aec405940f0155b576
trzcinsk <kristof.trz@gmail.com>
2015-01-07 11:59:57 +0000

    Implemented inner class

--- src/app/LibraryControl.java
+++ src/app/LibraryControl.java
@@ -79,5 +79,33 @@ public class LibraryControl {
 	private void printMagazines() {
 		LibraryUtils.printMagazines(library);
 	}
+	
+	private enum Option {
+		EXIT(0, "Exit the program"), ADD_BOOK(1, "Add new book"), ADD_MAGAZINE(2,
+				"Add new magazine"), PRINT_BOOKS(3, "Print available books"), PRINT_MAGAZINES(
+				4, "Print available magazines");
 
+		private int value;
+		private String description;
+
+		Option(int value, String description) {
+			this.value = value;
+			this.description = description;
+		}
+
+		@Override
+		public String toString() {
+			return value + " " + description;
+		}
+
+		public static Option createFromInt(int option) throws NoSuchElementException {
+			Option result = null;
+			try {
+				result = Option.values()[option];
+			} catch (ArrayIndexOutOfBoundsException e) {
+				throw new NoSuchElementException("Invalid element");
+			}
+			return result;
+		}
+	}
 }
